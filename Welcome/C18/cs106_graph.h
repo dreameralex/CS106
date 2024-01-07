a65cf62b96b7049d4f74dbf59d5e91d00693715f
Alex <zhangyh8216@sina.com>
2024-01-07 18:36:55 +0800

    publish at 2024-1-7, add graph, there still bugs in getNeighbors

--- Welcome/C18/cs106_graph.h
+++ Welcome/C18/cs106_graph.h
@@ -3,6 +3,11 @@
 #include <string>
 #include "set.h"
 #include "map.h"
+#include "queue.h"
+#include <iostream>
+using namespace std;
+
+
 
 template <typename NodeType, typename ArcType>
 class Cs106_graph
@@ -50,14 +55,24 @@ public:
     bool isConnected(NodeType *n1, NodeType *n2) const;
 
     Set<NodeType *> & getNodeSet();
+    Set<ArcType *> & getArcSet();
+    Set<ArcType *> & getArcSet(NodeType *node);
+    Set<ArcType *> & getArcSet(std::string name);
 
-    Set<NodeType *> & getArcSet();
-    Set<NodeType *> & getArcSet(NodeType *node);
-    Set<NodeType *> & getArcSet(std::string name);
 
+    Set<NodeType *> & getNeighbors(NodeType *node);
+    Set<NodeType *> & getNeighbors(std::string node);
 
-    Set<NodeType *> & getNeighbores(NodeType *node);
-    Set<NodeType *> & getNeighbores(std::string name);
+    /*
+     * Function: depth First Search
+     * Usage: deepthFirstSearch(node);
+    */
+    void depthFirstSearch(NodeType *node);
+    void visitUsingDFS(NodeType *node, Set<NodeType *> & visited);
+    void visit(NodeType *node);
+
+    void breadthFirstSearch(NodeType *node);
+
 
 
     /*
@@ -132,12 +147,57 @@ NodeType *Cs106_graph<NodeType,ArcType>::addNode(NodeType *node){
     return node;
 }
 
+/*
+ * Implateation notes: removeNode
+ * The removeNode method must remove the specified node but must
+ * also remove any arcs in the graph containin the node. To avoid
+ * changing the node set during iteration, this implementation creates
+ * a vector of arcs that require deletion.
+*/
+template <typename NodeType, typename ArcType>
+void Cs106_graph<NodeType,ArcType>::removeNode(std::string name){
+    removeNode(getExistingNode(name));
+}
+
+template <typename NodeType, typename ArcType>
+void Cs106_graph<NodeType,ArcType>::removeNode(NodeType *node){
+    Vector<ArcType *> toRemove;
+    for(ArcType *arc:arcs){
+        if (arc->start == node || arc->finish == node){
+            toRemove.add(arc);
+        }
+    }
+    for (ArcType *arc: toRemove){
+        removeArc(arc);
+    }
+    nodes.remove(node);
+}
+
+/*
+ * Implementation notes: getNode, getExistingNode
+ * Look up node.
+*/
+
+template <typename NodeType, typename ArcType>
+NodeType *Cs106_graph<NodeType,ArcType>::getNode(std::string name) const{
+    return nodeMap.get(name);
+}
+
 template <typename NodeType, typename ArcType>
 NodeType *Cs106_graph<NodeType,ArcType>::getExistingNode(std::string name) const{
     NodeType *node = nodeMap.get(name);
     if(node == NULL) error("No node named " + name);
     return node;
 }
+
+
+
+
+/*
+ * Implateation notes: add Arc
+ * The addArc method appears in three forms, as described in the interface.
+*/
+
 
 template <typename NodeType, typename ArcType>
 ArcType *Cs106_graph<NodeType,ArcType>::addArc(std::string s1, std::string s2){
@@ -159,7 +219,10 @@ ArcType *Cs106_graph<NodeType,ArcType>::addArc(ArcType *arc){
     return arc;
 }
 
-
+/*
+ * Implateation notes: removeArc
+ *
+*/
 
 template <typename NodeType, typename ArcType>
 void Cs106_graph<NodeType,ArcType>::removeArc(std::string s1, std::string s2){
@@ -186,6 +249,10 @@ void Cs106_graph<NodeType,ArcType>::removeArc(ArcType *arc){
 
 }
 
+/*
+ * Implateation notes: isConnect
+ * Node n1 is connected to n2 if any of the arcs leaving n1 finish n2. *
+*/
 
 template <typename NodeType, typename ArcType>
 bool Cs106_graph<NodeType,ArcType>::isConnected(std::string s1, std::string s2) const{
@@ -200,8 +267,133 @@ bool Cs106_graph<NodeType,ArcType>::isConnected(NodeType *n1, NodeType *n2) cons
     return false;
 }
 
-template <typename NodeType, typename ArcType>
-Set<NodeType *> & Cs106_graph<NodeType,ArcType>::getArcSet(NodeType *node){
+/*
+ * Implateation notes: getNodeSet, get ArcSet
+ * These methods simply return the set requested by the client. The
+ * sets are returned by reference for efficiency, because doing so
+ * eliminates the need to copy the set.
+*/
 
-    return node.arcs;
+template <typename NodeType, typename ArcType>
+Set<NodeType *> & Cs106_graph<NodeType,ArcType>::getNodeSet(){
+    return nodes;
 }
+
+template <typename NodeType, typename ArcType>
+Set<ArcType *> & Cs106_graph<NodeType,ArcType>::getArcSet(){
+    return arcs;
+}
+
+template <typename NodeType, typename ArcType>
+Set<ArcType *> & Cs106_graph<NodeType,ArcType>::getArcSet(NodeType *node){
+    return node->arcs;
+}
+
+template <typename NodeType, typename ArcType>
+Set<ArcType *> & Cs106_graph<NodeType,ArcType>::getArcSet(std::string name){
+    return getArcSet(getExistingNode(name));
+}
+
+template <typename NodeType, typename ArcType>
+Set<NodeType *> & Cs106_graph<NodeType,ArcType>::getNeighbors(NodeType *node){
+    Set<NodeType *> nodesResult;
+    for(ArcType *arc : node->arcs){
+        nodesResult.add(arc->finish);
+    }
+    return nodesResult;
+}
+
+//There are bugsssssss here !!!!!!!!!!!!!! fuck!!!!!!!!!
+template <typename NodeType, typename ArcType>
+Set<NodeType *> & Cs106_graph<NodeType,ArcType>::getNeighbors(std::string node){
+
+    Set<NodeType *> nodesResult;
+    nodesResult = getNeighbors(getExistingNode(node));
+    //return getNeighbors(getExistingNode(node));
+    return nodesResult;
+}
+
+/*
+ * Implementation notes: copy constructor and assignment operator
+ * These methods ensure that copying a graph creates an entriely new
+ * parallel structure of nodes and arcs.
+ *
+*/
+template <typename NodeType, typename ArcType>
+const Cs106_graph<NodeType,ArcType> &
+        Cs106_graph<NodeType,ArcType>::operator =(const Cs106_graph & src){
+    if(this != & src){
+        clear();
+        deepCopy(src);
+    }
+    return *this;
+}
+
+template <typename NodeType, typename ArcType>
+void Cs106_graph<NodeType,ArcType>::deepCopy(const Cs106_graph & other){
+    for(NodeType *oldNode: other.nodes){
+        NodeType * newNode = new NodeType();
+        *newNode = *oldNode;
+        newNode->arcs.clear();
+        addNode(newNode);
+    }
+    for(ArcType *oldArc : other.arcs){
+        ArcType *newArc = new ArcType();
+        *newArc = * oldArc;
+        newArc->start = getExistingNode(oldArc->start->name);
+        newArc->finish = getExistingNode(oldArc->finish->name);
+        addArc(newArc);
+    }
+}
+
+
+
+
+
+
+
+
+/*
+ * Implementation notes: Using DFS to search whole graph
+*/
+
+template <typename NodeType, typename ArcType>
+void Cs106_graph<NodeType,ArcType>::depthFirstSearch(NodeType *node){
+    Set<NodeType *> visited;
+    visitUsingDFS(node, visited);
+}
+
+template <typename NodeType, typename ArcType>
+void Cs106_graph<NodeType,ArcType>::visitUsingDFS(NodeType *node, Set<NodeType *> & visited){
+    if(visited.contains(node)) return;
+    visit(node);
+    visited.add(node);
+    for(ArcType *arc: node->arcs){
+        visitUsingDFS(arc->finish, visited);
+    }
+}
+
+template <typename NodeType, typename ArcType>
+void Cs106_graph<NodeType,ArcType>::visit(NodeType *node){
+    cout << node->name << " ";
+}
+
+
+
+template <typename NodeType, typename ArcType>
+void Cs106_graph<NodeType,ArcType>::breadthFirstSearch(NodeType *node){
+    Set<NodeType *> visited;
+    Queue<NodeType *> queue;
+    queue.enqueue(node);
+    while(!queue.isEmpty()){
+        node = queue.dequeue();
+        if(!visited.contains(node)){
+            visit(node);
+            visited.add(node);
+            for(ArcType *arc : node->arcs){
+                queue.enqueue(arc->finish);
+            }
+        }
+    }
+}
+
--- Welcome/C18/test_Cs106_graph.cpp
+++ Welcome/C18/test_Cs106_graph.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include "cs106_graph.h"
 #include "graph.h"
+#include "queue.h"
 
 using namespace std;
 
@@ -41,14 +42,52 @@ int main(){
     cout << "Add Node" << endl;
     g1.addNode("alex");
     g1.addNode("tom");
+    g1.addNode("gavin");
+    cout << g1.size() << endl;
+    g1.removeNode("gavin");
     cout << g1.size() << endl;
     cout << endl;
 
 
     cout << "Add arc" << endl;
     g1.addArc("alex","tom");
+    g1.removeArc("alex","tom");
+    g1.addArc("alex","tom");
+
+    g1.addNode("gavin1");
+    g1.addArc("gavin1","tom");
+    g1.addNode("mark");
+    g1.addArc("mark","tom");
+
+    g1.addArc("tom","mark");
+    g1.addArc("tom","alex");
     cout << endl;
 
+    cout << "Test get Node" << endl;
+    Set<NodeType *> test_node;
+    test_node = g1.getNodeSet();
+    cout << test_node.size();
+    Set<ArcType *> test_arcs;
+    test_arcs = g1.getArcSet();
+    cout << endl;
+
+    cout << "Test get neighbors" << endl;
+    Set<NodeType *> test_neighbors_node;
+
+
+    cout << "test ";
+    cout << "There are bugs in getNeighbors" << endl;
+    //cout << g1.getNeighbors("tom")    ;
+
+    cout << "Test Copy" << endl;
+    Cs106_graph<NodeType,ArcType> g2;
+    g2=g1;
+    cout << "copy graph size is : "<< g2.size();
+
+    cout << "Test serach:" << endl;
+
+    NodeType *alex_node = g1.getNode("alex");
+    g1.breadthFirstSearch(alex_node);
 
 
 
